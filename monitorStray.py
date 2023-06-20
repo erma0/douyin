ccce35e0d0ef3169c4a8845cf3246f9b80e5807d
erma0 <7407696+erma0@user.noreply.gitee.com>
2023-06-20 22:11:38 +0800

    新增 下载功能开关

--- monitorStray.py
+++ monitorStray.py
@@ -34,64 +34,77 @@ def get_path(relative_path):
 
 def start() -> None:
     print('任务开始')
+    edge = Browser()
     with open('url.txt', 'r', encoding='utf-8') as f:
         lines: list[str] = f.readlines()
     for url in lines:
         a = API(edge.context, url)
         a.run()
         if a.results:
-            icon.notify(f"发现{len(a.results)}条新内容，开始下载。", "发现新内容")
-            a.download()
+            if download:
+                icon.notify(f"发现{len(a.results)}条新内容，开始下载", "发现新内容")
+                a.download()
+            else:
+                icon.notify(f"发现{len(a.results)}条新内容", "发现新内容")
+        else:
+            # icon.notify("未发现新内容")
+            pass
+    edge.stop()
     print('任务结束')
 
 
-def click_menu(icon: Icon, item: MenuItem):
-    global state
-    state = not item.checked
+def click_download(icon: Icon, item: MenuItem):
+    global download
+    download = not item.checked
+
+
+def click_start(icon: Icon, item: MenuItem):
+    global running
+    running = not item.checked
     set_schedule(icon)
 
 
-def on_exit(icon: Icon, item: MenuItem):
-    global state, run
+def click_exit(icon: Icon, item: MenuItem):
+    global state
     state = False
-    run = False
     schedule.clear()
     icon.stop()
 
 
 def set_schedule(icon: Icon):
-    if state:
+    if running:
         schedule.every(10).minutes.do(start)
-        global first
-        first = True
+        schedule.run_all()
         if icon.HAS_NOTIFICATION:
             icon.title = '监控中'
             icon.notify(f'{int(schedule.idle_seconds())}秒后执行下一次任务', '抖音监控')
     else:
+        schedule.clear()
         if icon.HAS_NOTIFICATION:
             icon.title = '未启动'
             icon.notify("停止监控", '抖音监控')
-        schedule.clear()
 
 
-edge = Browser()
-state = True
-first = True
-run = True
-
-menu = (MenuItem(text='开始', action=click_menu, checked=lambda item: state, default=True), MenuItem(text='退出', action=on_exit))
-image: Image = Image.open(get_path("ico.ico"))
-icon: Icon = Icon("监控", image, "监控中" if state else "未启动", menu)
-threading.Thread(target=icon.run).start()
-time.sleep(1)
-
-set_schedule(icon)
-
-while run:
-    if first:
-        first = False
-        schedule.run_all()
-    else:
+def on_start():
+    set_schedule(icon)
+    while state:
         schedule.run_pending()
-    time.sleep(1)
-edge.stop()
+        time.sleep(1)
+
+
+state = True
+running = True
+download = False
+
+menu = (
+    MenuItem(text='开始', action=click_start, checked=lambda item: running, default=True),
+    MenuItem(text='下载', action=click_download, checked=lambda item: download, default=False),
+    MenuItem(text='退出', action=click_exit),
+)
+image: Image = Image.open(get_path("ico.ico"))
+icon: Icon = Icon("监控", image, "监控中" if running else "未启动", menu)
+
+# 使用icon.run(setup=on_start)时无法创建图标，只能单独跑一个线程
+threading.Thread(target=on_start, daemon=True).start()
+
+icon.run()
