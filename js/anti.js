dd8925b49fccf9604911fb38b57944514db3d733
erma0 <7407696+erma0@user.noreply.gitee.com>
2023-06-22 17:46:52 +0800

    新增 反检测js 备用

---
+++ js/anti.js
@@ -0,0 +1,88 @@
+window.chrome = {
+    "app": {
+        "isInstalled": false,
+        "InstallState": {
+            "DISABLED": "disabled",
+            "INSTALLED": "installed",
+            "NOT_INSTALLED": "not_installed"
+        },
+        "RunningState": {
+            "CANNOT_RUN": "cannot_run",
+            "READY_TO_RUN": "ready_to_run",
+            "RUNNING": "running"
+        }
+    },
+    "runtime": {
+        "OnInstalledReason": {
+            "CHROME_UPDATE": "chrome_update",
+            "INSTALL": "install",
+            "SHARED_MODULE_UPDATE": "shared_module_update",
+            "UPDATE": "update"
+        },
+        "OnRestartRequiredReason": {
+            "APP_UPDATE": "app_update",
+            "OS_UPDATE": "os_update",
+            "PERIODIC": "periodic"
+        },
+        "PlatformArch": {
+            "ARM": "arm",
+            "ARM64": "arm64",
+            "MIPS": "mips",
+            "MIPS64": "mips64",
+            "X86_32": "x86-32",
+            "X86_64": "x86-64"
+        },
+        "PlatformNaclArch": {
+            "ARM": "arm",
+            "MIPS": "mips",
+            "MIPS64": "mips64",
+            "X86_32": "x86-32",
+            "X86_64": "x86-64"
+        },
+        "PlatformOs": {
+            "ANDROID": "android",
+            "CROS": "cros",
+            "LINUX": "linux",
+            "MAC": "mac",
+            "OPENBSD": "openbsd",
+            "WIN": "win"
+        },
+        "RequestUpdateCheckStatus": {
+            "NO_UPDATE": "no_update",
+            "THROTTLED": "throttled",
+            "UPDATE_AVAILABLE": "update_available"
+        }
+    }
+};
+
+
+Object.defineProperties(navigator, {
+    webdriver: {
+        get: () => undefined
+    },
+    plugins: {
+        get: () => [{
+            0: {
+                type: "application/x-google-chrome-pdf",
+                suffixes: "pdf",
+                description: "Portable Document Format",
+                enabledPlugin: Plugin
+            },
+            description: "Portable Document Format",
+            filename: "internal-pdf-viewer",
+            length: 1,
+            name: "Chrome PDF Plugin"
+        }, {
+            0: {
+                type: "application/pdf",
+                suffixes: "pdf",
+                description: "",
+                enabledPlugin: Plugin
+            },
+            description: "",
+            filename: "mhjfbmdgcfjbbpaeojofohoefgiehjai",
+            length: 1,
+            name: "Chrome PDF Viewer"
+        }]
+    }
+});
