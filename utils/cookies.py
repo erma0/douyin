bf0a330cbb9644fab9a5cf18ff4fa480b91f97e4
erma0 <7407696+erma0@user.noreply.gitee.com>
2024-07-31 23:03:10 +0800

    新增cookies_dict_to_str

--- utils/cookies.py
+++ utils/cookies.py
@@ -15,16 +15,16 @@ def get_cookie_dict(cookie='') -> dict:
             cj = eval(f"browser_cookie3.{cookie}(domain_name='douyin.com')")
             cookie = requests.utils.dict_from_cookiejar(cj)
         else:
-            cookie = cookies_to_dict(cookie)
+            cookie = cookies_str_to_dict(cookie)
         save_cookie(cookie)
     elif os.path.exists('config/cookie.json'):
         with open('config/cookie.json', 'r', encoding='utf-8') as f:
             cookie = json.load(f)
     elif os.path.exists('config/cookie.txt'):
         with open('config/cookie.txt', 'r', encoding='utf-8') as f:
-            cookie = cookies_to_dict(f.read())
+            cookie = cookies_str_to_dict(f.read())
     else:
-        cookie = cookies_to_dict(input('请输入cookie:'))
+        cookie = cookies_str_to_dict(input('请输入cookie:'))
         save_cookie(cookie)
     return cookie
 
@@ -38,7 +38,7 @@ def test_cookie(cookie):
     if type(cookie) is dict:
         cookie_dict = cookie
     elif type(cookie) is str:
-        cookie_dict = cookies_to_dict(cookie)
+        cookie_dict = cookies_str_to_dict(cookie)
 
     res = requests.get(url, cookies=cookie_dict).json()
     if res['has_login'] is True:
@@ -49,7 +49,7 @@ def test_cookie(cookie):
         return False
 
 
-def cookies_to_dict(cookie_string: str) -> dict:
+def cookies_str_to_dict(cookie_string: str) -> dict:
     cookies = cookie_string.strip().split('; ')
     cookie_dict = {}
     for cookie in cookies:
@@ -58,6 +58,10 @@ def cookies_to_dict(cookie_string: str) -> dict:
         key, value = cookie.split('=', 1)
         cookie_dict[key] = value
     return cookie_dict
+
+
+def cookies_dict_to_str(cookie_string: dict) -> str:
+    return '; '.join([f'{key}={value}' for key, value in cookie_string.items()])
 
 
 if __name__ == "__main__":
